6aed9556a826c669c257e79dddc7ede22892280d
Pyrrait, Duarte <Duarte.Pyrrait@sbanken.no>
2020-06-09 15:19:59 +0100

    initial files

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-$(".showSwoosh").click(function() {
-  $(".swooshWrapper").toggleClass("is-hidden");
-});
+(function() {
+
+  document.querySelector('.showSwoosh').addEventListener("click", function(){
+    console.log('open');
+    document.getElementById('swooshWrapper').classList.add('visible');
+  });
+
+  document.querySelector('.hideSwoosh').addEventListener("click", function(){
+    console.log('close');
+    document.getElementById('swooshWrapper').classList.remove('visible');
+  });
+
+})();
